41272525e6892005f7d1c0b9bab7c8a5e16785af
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-06-10 18:19:54 +0200

    Make application run at interval and between specific times

--- src/app.js
+++ src/app.js
@@ -9,17 +9,41 @@ import { Application } from './application.js'
 import { sendErrorNotificationToDiscord } from './sendNotification.js'
 
 const main = async () => {
-  try {
-    if (!process.env.URL) {
-      throw new Error('Please enter a url in the .env file for URL variable')
-    }
+  const hour = new Date().getHours()
+  const startHour = parseInt(process.env.START_HOUR)
+  const endHour = parseInt(process.env.END_HOUR)
 
+  if (hour < startHour || hour >= endHour) {
+    return
+  }
+
+  try {
     // Begin to run the actual application.
     const application = new Application(process.env.URL)
     await application.run()
   } catch (error) {
-    sendErrorNotificationToDiscord(error)
+    await sendErrorNotificationToDiscord(error)
   }
 }
 
+const intervalMinutes = parseInt(process.env.CHECK_INTERVAL)
+
 main()
+
+// Call main again at interval
+setInterval(main, intervalMinutes * 60 * 1000)
+
+// Send notification about crashes
+process.on('uncaughtException', async (error) => {
+  await sendErrorNotificationToDiscord(
+    new Error(`Uncaught Exception: ${error.message}`)
+  )
+  process.exit(1)
+})
+
+process.on('unhandledRejection', async (reason) => {
+  await sendErrorNotificationToDiscord(
+    new Error(`Unhandled Rejection: ${reason}`)
+  )
+  process.exit(1)
+})
